13737b7a3bb3dc014e1c8ce89b6138ae0100620e
DrakeEvans <ecg.drake@gmail.com>
2018-09-27 16:16:52 -0400

    router and SinglePage view

---
+++ src/components/SingleRoom.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {ChatBucket} from './ChatBox/';
+
+const SingleRoom = (props) => {
+
+    return (
+      <div>
+        <div className="article-container">
+          <h1>Article Title</h1>
+          <iframe src="https://www.huffingtonpost.com/entry/michael-avenatti-donald-trump-brett-kavanaugh_us_5bac5b68e4b082030e782124" frameborder="0"></iframe>
+        </div>
+         
+        
+      </div>
+    )
+  }
+
+export default SingleRoom
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
+import { Router } from "react-router-dom";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
