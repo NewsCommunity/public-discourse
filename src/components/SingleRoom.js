d74e3fb04d737d161c118a098431396830f93682
Matt <mintzer.chemistry@gmail.com>
2018-09-28 17:11:38 -0400

    CSS changes for ifram

--- src/components/SingleRoom.js
+++ src/components/SingleRoom.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import ChatBucket from '../containers/ChatBucket';
+import React, { Component } from 'react'
+import ChatBucket from '../containers/ChatBucket'
 
-const SingleRoom = (props) => {
-	return (
-		<div>
-			<div className="discourse-container">
-				<h1>publicDiscourse</h1>
-				<div className="iframe-container">
-					<iframe
-						src="https://www.huffingtonpost.com/entry/michael-avenatti-donald-trump-brett-kavanaugh_us_5bac5b68e4b082030e782124"
-						frameborder="0"
-					/>
-				</div>
-				<div className="footer">
-					<ChatBucket />
-				</div>
-			</div>
-		</div>
-	);
-};
+const SingleRoom = props => {
+  return (
+    <div>
+      <div className='discourse-container'>
+        <h1>publicDiscourse</h1>
+        <div className='single-room'>
+          <div className='iframe-container'>
+            <iframe src='https://www.huffingtonpost.com/entry/michael-avenatti-donald-trump-brett-kavanaugh_us_5bac5b68e4b082030e782124' />
+          </div>
+          <ChatBucket />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default SingleRoom;
+export default SingleRoom
