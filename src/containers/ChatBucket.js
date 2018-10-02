6468b368efc4b6b95fd39f808979a28baa8486ad
DrakeEvans <31104161+DrakeEvans@users.noreply.github.com>
2018-10-02 11:26:16 -0400

    Update ChatBucket.js

--- src/containers/ChatBucket.js
+++ src/containers/ChatBucket.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { connect } from 'react-redux';
 import { subscribeToDatabase_THUNK, postToDatabase } from '../state/chatRoom/actions';
 import { getChatMessages_THUNK, setChatMessages } from '../state/chatRoom';
@@ -19,23 +19,6 @@ function mapDispatch(dispatch) {
 		// postMsg: (msg) => dispatch(postMsg(msg)),
 		//subscribe: (chatRoomId, limit) => dispatch(subscribeToDatabase_THUNK(chatRoomId, limit))
 	};
-=======
-import { connect } from 'react-redux'
-import { subscribeToDatabase_THUNK, postToDatabase } from '../state/chatRoom/actions'
-import ChatBucket from '../components/ChatBox/ChatBucket'
-
-function mapState (state) {
-  return {
-    msgArray: state.ChatReducer.messages
-  }
-}
-
-function mapDispatch (dispatch) {
-  return {
-    postMsg: msg => dispatch(postToDatabase(msg)),
-    subscribe: (chatRoomId, limit) => dispatch(subscribeToDatabase_THUNK(chatRoomId, limit))
-  }
->>>>>>> 729b8d1742a375601acf9c83c77d8addff2e0028
 }
 
 export default connect(mapState, mapDispatch)(ChatBucket)
