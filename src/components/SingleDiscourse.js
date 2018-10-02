ae3ed73510a44556a3e63fc8477d371b2b275d13
DrakeEvans <ecg.drake@gmail.com>
2018-10-02 15:01:12 -0400

    single discourse, router changes, articleList, added discourseId to Chat Props

--- src/components/SingleDiscourse.js
+++ src/components/SingleDiscourse.js
@@ -1,65 +1,56 @@
-import React, { Component } from 'react'
-import ChatBucket from '../containers/ChatBucket'
-import { thunkGetSingleDiscourse } from '../state/discourse/actions';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import ChatBucket from "../containers/ChatBucket";
+import { thunkGetSingleDiscourse } from "../state/discourse/actions";
+import { connect } from "react-redux";
 
 class SingleDiscourse extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
-  
+
   componentDidMount = async () => {
-    console.log('SingleDiscourse did mount')
-    console.log(this.props.match.params.docId)
-    await this.props.getSingleDiscourse(this.props.match.params.docId)
- }
+    await this.props.getSingleDiscourse(this.props.match.params.docId);
+  };
 
   render() {
-    const { discourse } = this.props
+    const { discourse } = this.props;
     if (discourse.article) {
-      console.log('discourse', discourse)
-      const { url, title, } = discourse.article
-      console.log('url', url)
-    return (
-      <div>
-        <div className='discourse-container'>
-          <h1>publicDiscourse: {title}</h1>
-          <div className='single-room'>
-            <div className='iframe-container'>
-              <iframe src={url} />
+      const { url, title } = discourse.article;
+      return (
+        <div>
+          <div className="discourse-container">
+            <h1>publicDiscourse: {title}</h1>
+            <div className="single-room">
+              <div className="iframe-container">
+                <iframe src={url} />
+              </div>
+              <ChatBucket discourseId={discourse.docId} />
             </div>
-            <ChatBucket discourseId={discourse.docId} />
           </div>
         </div>
-      </div>
-    )
-
+      );
     }
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-}
+    return <div>Loading...</div>;
+  }
 }
 
 //CONTAINER===============================
 
-function mapState (state) {
+function mapState(state) {
   return {
     discourse: state.discourseReducer.discourse
-  }
+  };
 }
 
-function mapDispatch (dispatch) {
+function mapDispatch(dispatch) {
   return {
     getSingleDiscourse: (discourseId) => {
-      console.log('invoking getsingleDiscourse')
-      dispatch(thunkGetSingleDiscourse(discourseId))
+      dispatch(thunkGetSingleDiscourse(discourseId));
     }
-  }
+  };
 }
 
-
-
-export default SingleDiscourse = connect(mapState, mapDispatch)(SingleDiscourse)
+export default (SingleDiscourse = connect(
+  mapState,
+  mapDispatch
+)(SingleDiscourse));
