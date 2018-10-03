98bff8125752485969148525abfd6200bddd2b6d
Matt <mintzer.chemistry@gmail.com>
2018-10-03 14:44:45 -0400

    Change anchor tag in navbar to react Link

--- src/components/DiscourseList.js
+++ src/components/DiscourseList.js
@@ -11,16 +11,24 @@ export default class DiscourseList extends Component {
     return (
       <div className='discourse-list'>
         {discourseList.map(elem => {
-          return (
-            <DiscourseCard
-              discourseId={elem.docId}
-              discourseTitle={elem.article.title}
-              discourseDescription={elem.article.content}
-              discourseImg={elem.article.urlToImage}
-              discoursePublishedAt={elem.article.publishedAt}
-              key={elem.docId}
-                        />
-          )
+          if (elem.article) {
+            return (
+              <DiscourseCard
+                discourseId={elem.docId}
+                discourseTitle={elem.article.title}
+                discourseDescription={elem.article.content}
+                discourseImg={elem.article.urlToImage}
+                discoursePublishedAt={elem.article.publishedAt}
+                key={elem.docId}
+                            />
+            )
+          } else {
+            return (
+              <div>
+                                Loading...
+                            </div>
+            )
+          }
         })}
       </div>
     )
