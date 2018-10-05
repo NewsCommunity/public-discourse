a40f1359471976292bc3c94fbaef4c9857cd5514
Dennison Bertram <dennison@dennisonbertram.com>
2018-10-05 12:55:12 -0400

    Start working with BigNumber

--- src/components/ethereum/BlockChainBar.js
+++ src/components/ethereum/BlockChainBar.js
@@ -97,9 +97,13 @@ class BlockChainBucket extends Component {
 
 
 	async componentDidMount() {
+
+    this.props.setEthProvider();
+
     console.log("The State of User: ", this.props);
     console.log("The state of dispatch", this.props.setEthProvider);
-    
+
+    console.log("HELLO!")
     axios.get(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${etherscan}`)
       .then(res => {
         const rate = res.data.result.ethusd;
