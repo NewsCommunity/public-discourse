459b2d8faf9e479302991ddc239aee72fdd618d8
Dennison Bertram <dennisonbertram@Dennisons-MacBook-Pro.local>
2018-10-08 19:28:33 -0400

    Log in for condition chat working

--- src/components/BottomNavigation/BottomNav.js
+++ src/components/BottomNavigation/BottomNav.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Icon from '@material-ui/core/Icon';
 
-
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit,
@@ -16,9 +15,27 @@ const styles = theme => ({
 });
 
 function BottomNav(props) {
-  const { classes, onShowToggle, isOpen } = props;
+  const {
+    classes, onShowToggle, isOpen, isLoggedIn, logInUser,
+  } = props;
   return (
     <div className="Bottom-Nav">
+      {(isLoggedIn && !isOpen) ? (
+        <span />
+      ) : (
+        <React.Fragment>
+          <Button
+            variant="contained"
+            color="secondary"
+            aria-label="Edit"
+			className={classes.button}
+			onClick={logInUser}
+          >
+            Log in to Chat
+          </Button>
+        </React.Fragment>
+      )}
+
       {isOpen ? (
         <React.Fragment>
           <Button
