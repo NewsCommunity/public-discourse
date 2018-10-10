d60a8c2fdc059834e17a1bafc956ee57dc0eb0ce
DrakeEvans <ecg.drake@gmail.com>
2018-10-10 17:09:25 -0400

    fixed button not working on firefox

--- src/components/DiscourseCard.js
+++ src/components/DiscourseCard.js
@@ -54,8 +54,7 @@ function ImgMediaCard(props) {
       </CardActionArea>
       <CardActions>
         <DelayToolTips discourseId={discourseId} />
-        <Button size="small" color="primary">
-          <Link to={`/discourse/${discourseId}`}>
+        <Button component={Link} to={`/discourse/${discourseId}`} size="small" color="primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -63,9 +62,8 @@ function ImgMediaCard(props) {
               viewBox="0 0 24 24"
             >
               <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
-            </svg>{" "}
+            </svg>{"  "}
             Chat about this
-          </Link>
         </Button>
       </CardActions>
     </Card>
