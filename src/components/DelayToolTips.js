748ce7cfae2438c4337ed8c6e58ff7898f055cb0
Matt <mintzer.chemistry@gmail.com>
2018-10-10 15:16:41 -0400

    Add svg icons for chat and copy to clipboard buttons on discourse cards

--- src/components/DelayToolTips.js
+++ src/components/DelayToolTips.js
@@ -32,7 +32,7 @@ class TriggersTooltips extends React.Component {
                   }}
                   onClose={this.handleTooltipClose}
                   open={this.state.open}
-                  title='Added to clipboard'
+                  title='Copied to clipboard'
                   leaveDelay={100}
                                 >
                   <Button
@@ -43,8 +43,16 @@ class TriggersTooltips extends React.Component {
                                             )
                       }}
                                     >
-                                        Copy link to clipboard
-                                    </Button>
+                    {/* THIS IS THE SVG ICON FOR THE CLIPBOARD */}
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                                        >
+                        <path d='M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z' />
+                      </svg>
+                  </Button>
                 </Tooltip>
               </div>
             </ClickAwayListener>
