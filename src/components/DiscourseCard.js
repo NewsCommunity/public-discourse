d604c2b1c48bf808a56c7d8f13656a473f2c36df
DrakeEvans <ecg.drake@gmail.com>
2018-10-13 15:15:08 -0400

    fixed broken image rendering

--- src/components/DiscourseCard.js
+++ src/components/DiscourseCard.js
@@ -32,45 +32,49 @@ function ImgMediaCard(props) {
     discourseImg,
     classes
   } = props;
-  return (
-    <Card className={classes.card}>
-      <CardActionArea component={Link} to={`/discourse/${discourseId}`}>
-        <CardMedia
-          component="img"
-          alt="Article Thumbnail"
-          className={classes.media}
-          height="100%"
-          image={`${discourseImg}`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h8" component="h6">
-            {discourseTitle}
-          </Typography>
-          <Typography component="p">{discourseDescription}</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <DelayToolTips discourseId={discourseId} />
-        <Button
-          component={Link}
-          to={`/discourse/${discourseId}`}
-          size="small"
-          color="primary"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+  if (discourseImg === "") {
+    return null;
+  } else {
+    return (
+      <Card className={classes.card}>
+        <CardActionArea component={Link} to={`/discourse/${discourseId}`}>
+          <CardMedia
+            component="img"
+            alt="Article Thumbnail"
+            className={classes.media}
+            height="100%"
+            image={`${discourseImg}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h8" component="h6">
+              {discourseTitle}
+            </Typography>
+            <Typography component="p">{discourseDescription}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <DelayToolTips discourseId={discourseId} />
+          <Button
+            component={Link}
+            to={`/discourse/${discourseId}`}
+            size="small"
+            color="primary"
           >
-            <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
-          </svg>
-          {"  "}
-          Chat about this
-        </Button>
-      </CardActions>
-    </Card>
-  );
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
+            </svg>
+            {"  "}
+            Chat about this
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
 
 ImgMediaCard.propTypes = {
